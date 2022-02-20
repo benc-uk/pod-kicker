0b27398b79b4a0548adf972d293d921c32c894e5
Ben Coleman <benc.uk@gmail.com>
2022-02-20 15:08:02 +0000

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/radovskyb/watcher"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+var (
+	version      = "0.0.0"            // App version number, set at build time with -ldflags "-X main.version=1.2.3"
+	buildInfo    = "No build details" // Build details, set at build time with -ldflags "-X main.buildInfo='Foo bar'"
+	isRestarting = false
+)
+
+func main() {
+	log.Printf("### 🚀 PodKicker %s starting...", version)
+
+	watchDir := os.Getenv("PODKICKER_WATCH_DIR")
+	if watchDir == "" {
+		log.Fatalln("### 💥 Env var PODKICKER_WATCH_DIR was not set")
+	}
+	targetName := os.Getenv("PODKICKER_TARGET_NAME")
+	if targetName == "" {
+		log.Fatalln("### 💥 Env var PODKICKER_TARGET_NAME was not set")
+	}
+	targetType := os.Getenv("PODKICKER_TARGET_TYPE")
+	if targetType == "" {
+		targetType = "deployment"
+	}
+
+	// Connect to Kubernetes API
+	log.Printf("### 🌀 Attempting to use in cluster config")
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("### 💻 Connecting to Kubernetes API, using host: %s", config.Host)
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	namespaceBytes, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	if err != nil {
+		log.Fatal(err)
+	}
+	namespace := string(namespaceBytes)
+
+	fileWatcher := watcher.New()
+
+	go func() {
+		for {
+			select {
+			case event := <-fileWatcher.Event:
+				// Prevent multiple restarts if multiple file events happen at once
+				if isRestarting {
+					return
+				}
+
+				log.Printf("### ⛔ Detected file change: %v", event)
+
+				patchData := fmt.Sprintf(`{"spec":{"template":{"metadata":{"annotations":{"kubectl.kubernetes.io/restartedAt":"%s"}}}}}`, time.Now().Format(time.RFC3339))
+
+				var err error
+				if strings.ToLower(targetType) == "statefulset" {
+					_, err = clientset.AppsV1().StatefulSets(namespace).Patch(context.Background(), targetName, types.StrategicMergePatchType, []byte(patchData), metav1.PatchOptions{FieldManager: "kubectl-rollout"})
+				} else {
+					_, err = clientset.AppsV1().Deployments(namespace).Patch(context.Background(), targetName, types.StrategicMergePatchType, []byte(patchData), metav1.PatchOptions{FieldManager: "kubectl-rollout"})
+				}
+
+				if err != nil {
+					log.Printf("### 👎 Warning: Failed to patch %s, restart failed: %v", targetType, err)
+				} else {
+					isRestarting = true
+					log.Printf("### ✅ Target %s, named %s was restarted!", targetType, targetName)
+				}
+			case err := <-fileWatcher.Error:
+				log.Fatalln(err)
+			case <-fileWatcher.Closed:
+				return
+			}
+		}
+	}()
+
+	// Watch this folder for changes.
+	if err := fileWatcher.AddRecursive(watchDir); err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("### 👀 Watching directory '%s' ...", watchDir)
+
+	// Start the watching process - it'll check for changes every 200ms.
+	if err := fileWatcher.Start(time.Millisecond * 200); err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ makefile
@@ -0,0 +1,48 @@
+# Common variables
+VERSION := 0.0.1
+BUILD_INFO := Manual build 
+SRC_DIR := cmd
+
+# Most likely want to override these when calling `make image`
+IMAGE_REG ?= ghcr.io
+IMAGE_REPO ?= benc-uk/pod-kicker
+IMAGE_TAG ?= latest
+IMAGE_PREFIX := $(IMAGE_REG)/$(IMAGE_REPO)
+
+# Things you don't want to change
+REPO_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+GOLINT_PATH := $(REPO_DIR)/bin/golangci-lint # Remove if not using Go
+
+.PHONY: help image push build run lint lint-fix
+.DEFAULT_GOAL := help
+
+help: ## 💬 This help message :)
+	@figlet $@ || true
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-20s\033[0m %s\n", $$1, $$2}'
+
+lint: ## 🌟 Lint & format, will not fix but sets exit code on error
+	@figlet $@ || true
+	@$(GOLINT_PATH) > /dev/null || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh
+	cd $(SRC_DIR); $(GOLINT_PATH) run --modules-download-mode=mod *.go
+
+lint-fix: ## 🔍 Lint & format, will try to fix errors and modify code
+	@figlet $@ || true
+	@$(GOLINT_PATH) > /dev/null || curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh
+	cd $(SRC_DIR); golangci-lint run --modules-download-mode=mod *.go --fix
+
+image: ## 📦 Build container image from Dockerfile
+	@figlet $@ || true
+	docker build --file ./build/Dockerfile \
+	--build-arg BUILD_INFO="$(BUILD_INFO)" \
+	--build-arg VERSION="$(VERSION)" \
+	--tag $(IMAGE_PREFIX):$(IMAGE_TAG) . 
+
+push: ## 📤 Push container image to registry
+	@figlet $@ || true
+	docker push $(IMAGE_PREFIX):$(IMAGE_TAG)
+
+build: ## 🔨 Run a local build without a container
+	@figlet $@ || true
+	@echo "Not implemented yet!"
+	#go build -o __CHANGE_ME__ $(SRC_DIR)/...
+	#cd $(SRC_DIR); npm run build
